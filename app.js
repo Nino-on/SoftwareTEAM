7ddd5e4f01690f38e9ea85a90474f20d3992139a
Ren <raulnino24@gmail.com>
2020-03-11 18:38:45 -0400

    updated

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
-const Manager = require(".lib/Manager");
+const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
-const Intern = require(".lib/Intern");
+const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 const question = require("./lib/question");
 
 const render = require("./lib/htmlRenderer");
+start()
+function start() {
+    inquirer.prompt(question.askManager).then(function (data) {
+        menu()
+    })
+}
+
+
+
+function menu() {
+    inquirer.prompt(question.typeEmployee).then(function (data) {
+        switch(data.choices){
+            case "Engineer":
+                inquirer.prompt(question.forEngineer).then(function (data) {
+                    menu()
+                })   
+                break;
+            case "Intern":
+                inquirer.prompt(question.forIntern).then(function (data) {
+                    menu()
+                })
+                break;
+            case "Exit":
+                console.log("succesfully")
+                break;
+                
+            
+        }
+    })
+}
